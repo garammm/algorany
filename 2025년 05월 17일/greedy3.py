866b4133add1fb27dab91075f2d0466582d58cfb
Garam Yu <garam010201@naver.com>
2025-05-17 16:10:10 +0900

    숫자 카드 게임 (0517)

--- 2025년 05월 17일/greedy3.py
+++ 2025년 05월 17일/greedy3.py
@@ -1,4 +1,10 @@
+# Greedy 3번 문제 -> 책에 있는 정답 코드 그러나 이해함! 그리고 스스로 적어봄!(안보고!!)
 n,m = map(int,input().split())
 
+answer = 0
 for _ in range(n):
-    
+    n_list = list(map(int, input().split()))
+    min_num = min(n_list)
+    answer = max(min_num, answer)
+
+print(answer)
