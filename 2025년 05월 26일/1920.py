e14bf62d446db2f76e1c4a6467ae84788af88489
Garam Yu <garam010201@naver.com>
2025-05-26 19:23:28 +0900

    이진탐색법_예시문제

--- 2025년 05월 26일/1920.py
+++ 2025년 05월 26일/1920.py
@@ -1,3 +1,14 @@
+# 꼭 다시 풀어보기...아니다 이건 꼭... 내일 아침에 다시 꼭 다시... 피티 없이 풀어보자...
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start+end)//2
+        if array[mid] == target:
+            return True
+        elif array[mid] < target:
+            start = mid+1
+        else:
+            end = mid-1
+    return False
 
 n = int(input())
 a_list = list(map(int,input().split()))
@@ -5,11 +16,10 @@ m = int(input())
 b_list = list(map(int,input().split()))
 
 a_list.sort()
-b_list.sort()
 
-for a in a_list:
-    for b in b_list:
-        if b==a:
-            print("1")
-        elif b!=a:
-            print("0")
+
+for b in b_list:
+    if binary_search(a_list, b, 0, n-1):
+        print(1)
+    else:
+        print(0)
