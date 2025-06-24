428bf285b2c316be2cdbf080692ca372846593ce
Garam Yu <garam010201@naver.com>
2025-06-24 19:31:06 +0900

    20250624

--- 2025년 06월 23일/4889.py
+++ 2025년 06월 23일/4889.py
@@ -1,17 +1,40 @@
-string = input()
-a = len(string)
+def answer(string):
+    stack = [] 
 
-count_left = 0
-count_right = 0
-answer_count = 0
-stack = [] 
+    for char in string:
+        if char == '{':
+            stack.append(char)
+        elif char == '}':
+            if stack:                   # stack에 뭔가 있을 때
+                stack.pop()
+            else:
+                stack.append(char)
+                pass          # 짝이 안맞으면 일단 stack에 넣는다.
 
-for char in string:
-    if char == '{':
-        stack.append(char)
-    else:                           # 닫는 괄호일 경우
-        if stack:                   # stack에 뭔가 있을 때
-            stack.pop()
-        else:
-            pass
-        
+    a = len(stack)
+    count_left = 0
+    count_right = 0
+    if a ==0:
+        return 0 
+
+    for char in stack:   
+        if char == "{":
+            count_left +=1
+        elif char == "}":
+            count_right+=1
+
+    # 2,4 까지 고려하고 6일 때를 고려하지 않음
+    '''
+    if count_right %2 ==1:
+        return (int(a//2+1))
+    else:
+        return (int(a//2))
+        '''
+    return (count_left +1)//2 + (count_right+1)//2
+
+
+while True:
+    string = input().rstrip()
+    if string[0] == '-':   # 종료 조건
+        break
+    print(answer(string))
